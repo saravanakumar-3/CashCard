215ce379a3fc6ad8764ee4a7dea3ddcf6c3539e4
Saravanakumar <saravanakumar.2995@gmail.com>
2024-06-17 13:26:20 +0530

    simple get request handling with TDD

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -1,4 +1,4 @@
-package com.example.cashcard;
+package example.cashcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,40 @@
+package example.cashcard;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContentAssert;
+
+@JsonTest
+public class CashCardJsonTest {
+  @Autowired private JacksonTester<CashCard> json;
+
+  @Test
+  public void cashCardSerializationTest() throws IOException {
+    CashCard cashCard = new CashCard(99L, 123.45);
+    JsonContentAssert jsonContentAssert = assertThat(json.write(cashCard));
+    jsonContentAssert.isStrictlyEqualToJson("expected.json");
+    jsonContentAssert.hasJsonPathNumberValue("@.id");
+    jsonContentAssert.extractingJsonPathNumberValue("@.id").isEqualTo(99);
+    jsonContentAssert.hasJsonPathNumberValue("@.amount");
+    jsonContentAssert.extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+  }
+
+  @Test
+  void cashCardDeserializationTest() throws IOException {
+    String expected =
+        """
+           {
+               "id":99,
+               "amount":123.45
+           }
+           """;
+    assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
+    assertThat(json.parseObject(expected).id()).isEqualTo(99);
+    assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+  }
+}
