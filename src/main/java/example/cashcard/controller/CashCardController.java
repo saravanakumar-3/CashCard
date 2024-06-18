dcef17b0553f2cf5790bab01880eadce55eb9e76
Saravanakumar <saravanakumar.2995@gmail.com>
2024-06-18 20:59:59 +0530

    implemented post and get list

--- src/main/java/example/cashcard/controller/CashCardController.java
+++ src/main/java/example/cashcard/controller/CashCardController.java
@@ -2,13 +2,18 @@ package example.cashcard.controller;
 
 import example.cashcard.model.CashCard;
 import example.cashcard.repository.CashCardRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import java.net.URI;
+import java.util.List;
 import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -22,10 +27,28 @@ public class CashCardController {
   @GetMapping("/{requestedId}")
   private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
     Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
-    if (cashCardOptional.isPresent()) {
-      return ResponseEntity.ok(cashCardOptional.get());
-    } else {
-      return ResponseEntity.notFound().build();
-    }
+    return cashCardOptional
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @GetMapping
+  private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+    Page<CashCard> page = cashCardRepository.findAll(
+            PageRequest.of(
+                    pageable.getPageNumber(),
+                    pageable.getPageSize(),
+                    pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+            ));
+    return ResponseEntity.ok(page.getContent());
+  }
+
+  @PostMapping
+  private ResponseEntity<CashCard> create(
+      @RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+    CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+    URI locationOfNewCashCard =
+        ucb.path("cashcards/{id}").buildAndExpand(savedCashCard.id()).toUri();
+    return ResponseEntity.created(locationOfNewCashCard).build();
   }
 }
--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -2,9 +2,10 @@ package example.cashcard;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
-
 import example.cashcard.model.CashCard;
+import java.io.IOException;
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -14,12 +15,22 @@ import org.springframework.boot.test.json.JsonContentAssert;
 @JsonTest
 public class CashCardJsonTest {
   @Autowired private JacksonTester<CashCard> json;
+  @Autowired private JacksonTester<CashCard[]> jsonList;
+
+  private CashCard[] cashCards;
+
+  @BeforeEach
+  void setUp() {
+    cashCards =
+        Arrays.array(
+            new CashCard(99L, 123.45), new CashCard(100L, 1.00), new CashCard(101L, 150.00));
+  }
 
   @Test
   public void cashCardSerializationTest() throws IOException {
     CashCard cashCard = new CashCard(99L, 123.45);
     JsonContentAssert jsonContentAssert = assertThat(json.write(cashCard));
-    jsonContentAssert.isStrictlyEqualToJson("expected.json");
+    jsonContentAssert.isStrictlyEqualToJson("single.json");
     jsonContentAssert.hasJsonPathNumberValue("@.id");
     jsonContentAssert.extractingJsonPathNumberValue("@.id").isEqualTo(99);
     jsonContentAssert.hasJsonPathNumberValue("@.amount");
@@ -38,5 +49,23 @@ public class CashCardJsonTest {
     assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
     assertThat(json.parseObject(expected).id()).isEqualTo(99);
     assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+  }
+
+  @Test
+  void cashCardListSerializationTest() throws IOException {
+    assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
+  }
+
+  @Test
+  void cashCardListDeserializationTest() throws IOException {
+    String expected =
+        """
+         [
+            { "id": 99, "amount": 123.45 },
+            { "id": 100, "amount": 1.00 },
+            { "id": 101, "amount": 150.00 }
+         ]
+         """;
+    assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
   }
 }
